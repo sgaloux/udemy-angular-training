aeae99e6bcbec0c2b046f6c89facc29c01c9e620
Sébastien Galoux <sebastien.galoux@gmail.com>
2018-10-11 20:56:24 +0200

    use canLoad guard to prevent training module for being loaded if not auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ const routes: Routes = [
   { path: '', component: WelcomeComponent },
   {
     path: 'training',
-    loadChildren: './training/training.module#TrainingModule'
+    loadChildren: './training/training.module#TrainingModule',
+    canLoad: [AuthGuard]
   }
 ];
 
